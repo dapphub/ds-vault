616e48983b092e1603e805733aa70258ed433299
Daniel Brockman <daniel@brockman.se>
2017-03-11 21:06:53 +0100

    dapp

--- Makefile
+++ Makefile
@@ -1,1 +1,2 @@
-default:; dapple test
+all:; dapp build
+test:; dapp test
--- src/vault.t.sol
+++ src/vault.t.sol
@@ -1,28 +1,22 @@
-/*
-   Copyright 2016 Nexus Development, LLC
+/// vault.t.sol -- tests for vault.sol and multivault.sol
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+// Copyright (C) 2016, 2017  Nexus Development, LLC
 
-       http://www.apache.org/licenses/LICENSE-2.0
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND (express or implied).
 
-pragma solidity ^0.4.9;
+pragma solidity ^0.4.8;
 
-import "dapple/test.sol";
+import "ds-test/test.sol";
 import "erc20/erc20.sol";
-import "./vault.sol";
+
 import "./multivault.sol";
 
-
-contract DSMultiVaultTest is Test {
+contract DSMultiVaultTest is DSTest {
     function testVault() {
         var token = new FakeToken();
         var vault1 = new DSMultiVault();
